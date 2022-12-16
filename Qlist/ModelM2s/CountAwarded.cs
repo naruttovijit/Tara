a15d0162a7e09c0cf25ab10ff5c128d8466e7b04
naruttovijit <narut.t@tropicaltech.co.th>
2022-12-16 15:57:42 +0700

    Update
    Add function auto get member assignment

---
+++ Qlist/ModelM2s/CountAwarded.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Qlist.ModelM2s
+{
+    public class CountAwarded
+    {
+        public int MemberId { get; set; }
+        public int Count { get; set; }
+    }
+}
